dda93c6a9199c141f7d496af867eb982de3e952b
Ebru Davutoglu <ebrudavutoglu@gmail.com>
2017-11-15 16:55:00 +0300

    move js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+var clean         = require('gulp-clean');
+var browserSync   = require('browser-sync');
+var reload        = browserSync.reload;
 
 var SOURCEPATH = {
   sassSource : 'tpl/scss/*.scss',
-  htmlSource : 'tpl/*.html'
+  htmlSource : 'tpl/*.html',
+  jsSource   : 'tpl/js/**'
 }
 var APPPATH = {
-  root : 'app/',
-  css : 'app/css',
-  js : 'app/js'
+  root       : 'app/',
+  css        : 'app/css',
+  js         : 'app/js'
 }
+
+gulp.task('clean-html', function(){
+  return gulp.src(APPPATH.root + '/*.html', {read: false, forse:true})
+    .pipe(clean());
+});
+gulp.task('clean-js', function(){
+  return gulp.src(APPPATH.js + '/*.js', {read: false, forse:true})
+    .pipe(clean());
+});
 gulp.task('sass', function(){
   return gulp.src(SOURCEPATH.sassSource)
     .pipe(autoprefixer())
@@ -20,7 +31,12 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('copy', function(){
+gulp.task('script',['clean-js'], function(){
+  gulp.src(SOURCEPATH.jsSource)
+    .pipe(gulp.dest(APPPATH.js));
+});
+
+gulp.task('copy', ['clean-html'], function(){
   gulp.src(SOURCEPATH.htmlSource)
     .pipe(gulp.dest(APPPATH.root));
 });
@@ -33,9 +49,10 @@ gulp.task('serve',['sass'], function(){
   });
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html', 'script', 'clean-js'], function() {
   gulp.watch([SOURCEPATH.sassSource],['sass']);
   gulp.watch([SOURCEPATH.htmlSource],['copy']);
+  gulp.watch([SOURCEPATH.jsSource],['script']);
 
 })
 
